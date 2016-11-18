79557dde69e6415bdae7cf880299d24f1a715954
TihomirTE <tihomirteofilov@gmail.com>
2016-11-18 23:44:31 +0200

    Add HW2 Methods

---
+++ C Sharp/C# Advanced/HW2_Methods/Task5_LargerThanNeighbours/LargerThanNeighbours.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_LargerThanNeighbours
+{
+    class LargerThanNeighbours
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            BiggerNeighbours(arr);
+        }
+
+        static void BiggerNeighbours(int[] arr)
+        {
+            int counter = 0;
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                if (arr[i] < arr[i + 1] && arr[i + 1] > arr[i + 2])
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
