212fc1d2f22d7d0a1c7d7140b65eac836fd6a506
TihomirTE <tihomirteofilov@gmail.com>
2016-11-24 23:30:42 +0200

    Add tasks

--- C Sharp/C# Advanced/HW2_Methods/Task8_NumberAsArray/NumberAsArray.cs
+++ C Sharp/C# Advanced/HW2_Methods/Task8_NumberAsArray/NumberAsArray.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Numerics;
-using System.Text;
 
 namespace Task8_NumberAsArray
 {
@@ -9,25 +7,63 @@ namespace Task8_NumberAsArray
     {
         static void Main()
         {
-            // var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            var nums = Console.ReadLine();
-            var arr1 = Console.ReadLine().Split(' ').ToString();
-            var arr2 = Console.ReadLine().Split(' ').ToString();
-            BigInteger num1 = ConvertArrayToNumber(arr1);
-            BigInteger num2 = ConvertArrayToNumber(arr2);
-            SumNumbers(num1, num2);
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var arr1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var arr2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int min = Math.Min(nums[0], nums[1]);
+            CalculateSum(min, arr1, arr2);
         }
-        static BigInteger ConvertArrayToNumber(string arr)
+
+        private static void CalculateSum(int minLenght, int[] arr1, int[] arr2)
         {
-            var reversedNum1 = arr.Reverse().ToString();
-            BigInteger number = BigInteger.Parse(reversedNum1);
-            return number;
+            int transfer = 0; // value (if the sum > 10) add one to the right position
+            int current = 0;
+            for (int i = 0; i < minLenght; i++) // Take the lenght of the smaller array
+            {
+                current = arr1[i] + arr2[i] + transfer; // Sum every position seperately
+                if (current > 9)
+                {
+                    current = current - 10;
+                    transfer = 1;
+                }
+                else
+                {
+                    transfer = 0;
+                }
+                Console.Write(current + " ");
+            }
+
+            // if one of the array is bigger than the other
+            if (arr1.Length > arr2.Length) 
+            {
+                CalculateTheRest(arr1, arr2, transfer);
+            }
+            else if (arr2.Length > arr1.Length)
+            {
+                CalculateTheRest(arr2, arr1, transfer);
+            }
+            // if the lenght of the arrays are the same && transfer == 1
+            else if (transfer == 1) 
+            {
+                Console.Write(1);
+            }
+            Console.WriteLine();
         }
-        static void SumNumbers(BigInteger number1, BigInteger number2)
+
+        private static void CalculateTheRest(int[] arrBigger, int[] arrSmaller, int transfer)
         {
-            BigInteger sum = number1 + number2;
-            var reversedSum = sum.ToString().Reverse();
-            Console.WriteLine(reversedSum);      
+            for (int i = arrSmaller.Length; i < arrBigger.Length; i++) // take the value of the bigger array
+            {
+                if (i == arrSmaller.Length) // include the transfer in the calculations
+                {
+                    Console.Write((arrBigger[i] + transfer) + " ");
+                }
+                else
+                {
+                    Console.Write(arrBigger[i] + " ");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ C Sharp/C# Advanced/HW4_ClassesAndObjects/Task5_TriangleThreeSide/TriangleThreeSide.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_TriangleThreeSide
+{
+    class TriangleThreeSide
+    {
+        static void Main()
+        {
+            double side1 = double.Parse(Console.ReadLine());
+            double side2 = double.Parse(Console.ReadLine());
+            double side3 = double.Parse(Console.ReadLine());
+
+            double halfPerimeter = (side1 + side2 + side3) / 2;
+            double area = Math.Sqrt(halfPerimeter * (halfPerimeter - side1)
+                * (halfPerimeter - side2) * (halfPerimeter - side3));
+            Console.WriteLine("{0:f2}", area);
+        }
+    }
+}
