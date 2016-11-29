55b61a04c51d2b3d7f3242560c878b4183e74db8
TihomirTE <tihomirteofilov@gmail.com>
2016-11-29 09:20:31 +0200

    Added some changes

--- C Sharp/C# Advanced/Workshop_ArraysMethodsClasses/Task2_BitShiftMatrix/BitShiftMatrix.cs
+++ C Sharp/C# Advanced/Workshop_ArraysMethodsClasses/Task2_BitShiftMatrix/BitShiftMatrix.cs
@@ -17,50 +17,26 @@ namespace Task2_BitShiftMatrix
             var codes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
             int coeff = Math.Max(rows, cols);
-            MoveThePawn(codes, coeff, numberMoves, rows, cols);
-
-        }
-
-        private static void MoveThePawn(int[] codes, int coeff, int moves, int rows, int cols)
-        {
-            int x = rows - 1;
-            int y = 0;
-            int number = 1;
-            bool visited = false;
-            var matrix = new int[rows, cols];
-            for (int i = 0; i < moves; i++)
+            
+            int sum = 0;
+            for (int i = 0; i < numberMoves; i++)
             {
-                int x1 = codes[i] / coeff;
-                int y1 = codes[i] % coeff;
-                int diffY = y1 - y;
-               
-                if (diffY >= 0)
-                {
-                    number = CalculateTheMove(diffY, number);
-                }
-                else
-                {
-                    number -= number >> diffY;
-                }
-                int diffX = x1 - x;
-                if (diffX >= 0)
-                {
-                    number += number << diffX;
-                }
-                else
-                {
-                    number -= number >> diffX;
-                }
+                sum += CalculateTheMove(codes[i], coeff);
             }
         }
 
-        private static int CalculateTheMove(int diff, int num)
+        static int CalculateTheMove(int move, int coeff)
         {
-            for (int i = 1; i <= diff; i++)
-            {
-                num += num << 1;
-            }
-            return num;
+            int row = move / coeff;
+            int col = move % coeff;
+
+            int currentSum = SumTheMovement(row, col);
+            return currentSum;
+        }
+
+        static int SumTheMovement(int row, int col)
+        {
+            
         }
     }
 }
