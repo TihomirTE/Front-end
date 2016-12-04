75b2787ad65af10a845d3dfb6677e2df49772789
TihomirTE <tihomirteofilov@gmail.com>
2016-12-04 23:06:19 +0200

    Add Methods

---
+++ C Sharp/C# Advanced/2-Methods/Task10_NFactorial/NFactorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace Task10_NFactorial
+{
+    class NFactorial
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            FindFactorial(number);
+        }
+
+        static void FindFactorial(int number)
+        {
+            BigInteger factorial = 1;
+            for (int i = number; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
