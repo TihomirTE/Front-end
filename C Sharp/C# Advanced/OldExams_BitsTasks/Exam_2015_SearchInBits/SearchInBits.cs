2755c6601ec464858e471a1e63866477b3edb097
TihomirTE <tihomirteofilov@gmail.com>
2016-12-06 22:22:31 +0200

    Bits tasks

---
+++ C Sharp/C# Advanced/OldExams_BitsTasks/Exam_2015_SearchInBits/SearchInBits.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_2015_SearchInBits
+{
+    class SearchInBits
+    {
+        static void Main()
+        {
+            int s = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            int pattern = Convert.ToString(s, 2).Length;
+            int occurrence = 0;
+            int tempNumber = 0;
+            int mask = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                
+                for (int j = 30; j >= pattern; j--)
+                {
+                    mask = s << (30 - j);
+                    tempNumber = (number & mask);
+                    if (tempNumber == mask)
+                    {
+                        occurrence++;
+                    }
+                }
+            }
+            Console.WriteLine(occurrence);
+        }
+    }
+}
