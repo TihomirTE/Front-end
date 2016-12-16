6f3778637a4d2343cc636f31213ec1c79be01816
TihomirTE <tihomirteofilov@gmail.com>
2016-12-16 10:36:31 +0200

    fixed sort3Numbers

--- Front-end/JavaScript-Fundamentals/03. Conditional Statements/04. Sort 3 numbers/sorting3Numbers.js
+++ Front-end/JavaScript-Fundamentals/03. Conditional Statements/04. Sort 3 numbers/sorting3Numbers.js
@@ -1,32 +1,33 @@
-function sort(args) {
+function solve(args) {
+    let str = args[0];
     let a = +(args[0]);
     let b = +(args[1]);
     let c = +(args[2]);
 
     if (a >= b) {
         if (a >= c && b >= c) {
-            console.log(a, b, c);
+            console.log(a + " " + b + " " + c);
         } else if (a >= c && c > b) {
-            console.log(a, c, b);
+            console.log(a + " " + c + " " + b);
         } else {
-            console.log(c, a, b);
+            console.log(c + " " + a + " " + b);
         }
     } else if (b > a) {
         if (b >= c && a >= c) {
-            console.log(b, a, c);
+            console.log(b + " " + a + " " + c);
         } else if (b >= c && c > a) {
-            console.log(b, c, a);
+            console.log(b + " " + c + " " + a);
         } else {
-            console.log(c, b, a);
+            console.log(c + " " + b + " " + a);
         }
     }
 }
 
 
-sort(['5', '1', '2']);
-sort(['-2', '-2', '1']);
+//solve([5, 1, 2]);
+/*ort(['-2', '-2', '1']);
 sort(['-2', '4', '3']);
 sort(['0', '-2.5', '5']);
 sort(['-1.1', '-0.5', '-0.1']);
 sort(['10', '20', '30']);
-sort(['1', '1', '1']);
+sort(['1', '1', '1']);*/
