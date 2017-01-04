1e87ad58549f5dc5d5ac2c5591465fd1d5c08100
TihomirTE <tihomirteofilov@gmail.com>
2017-01-04 09:31:10 +0200

    Exam - 05.03.2014

---
+++ C Sharp/OOP/07. Exam Preparation/01. Furniture - 05.03.2014/FurnitureManufacturer/Models/Chair.cs
@@ -0,0 +1,38 @@
+﻿using FurnitureManufacturer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureManufacturer.Models
+{
+    public class Chair : Furniture, IChair
+    {
+        private decimal height;
+        private string materialType;
+        private string model;
+        private decimal price;
+
+        public Chair(string model, MaterialType materialType, decimal price, decimal height, int numberOfLegs)
+        {
+            this.Model = model;
+            this.MaterialType = materialType;
+            this.Price = price;
+            this.Height = height;
+            this.NumberOfLegs = numberOfLegs;
+        }
+
+        public int NumberOfLegs { get; protected set; }
+
+        // TODO 
+
+        public override string ToString()
+        {
+            return string.Format("Type: {0}, Model: {1}, Material: {2}, Price: {3}, Heigth: {4}, Legs: {5}",
+                this.GetType().Name, this.Model, this.Material, this.Price,
+                this.Height, this.NumberOfLegs);
+        }
+
+    }
+}
