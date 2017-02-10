d858c88d604ce554189844cf77c9bedfcc439c48
TihomirTE <tihomirteofilov@gmail.com>
2017-02-10 16:50:56 +0200

    Add Task Cosmetics

---
+++ C Sharp/C# Unit Testing/03. WorkShops/01. Cosmetics/Solution/Cosmetics.Tests/Engine/CosmeticsEngineTests.cs
@@ -0,0 +1,50 @@
+﻿using Cosmetics.Contracts;
+using Cosmetics.Tests.Engine.Mocks;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Tests.Engine
+{
+    [TestFixture]
+    public class CosmeticsEngineTests
+    {
+        [Test]
+        public void Start_WhenTheInputIsInFormatInCreateCategoryCommand_ShouldReadParseAndExecuteCreateCategoryCommand()
+        {
+            // Arrange
+            var categoryName = "ForAnimals";
+
+            var stubFactory = new Mock<ICosmeticsFactory>();
+            var stubShoppingCart = new Mock<IShoppingCart>();
+            var stubCommandParser = new Mock<ICommandParser>();
+
+            var stubCommand = new Mock<ICommand>();
+            var stubCategory = new Mock<ICategory>();
+
+            // setup stubCommand
+            stubCommand.SetupGet(p => p.Name).Returns("CreateCategory");
+            stubCommand.SetupGet(p => p.Parameters).Returns(() => new List<string>() { categoryName });
+            // setup stubCommandParser
+            stubCommandParser.Setup(p => p.ReadCommands()).Returns(() => new List<ICommand>() { stubCommand.Object });
+
+            // expected result
+            stubCategory.SetupGet(c => c.Name).Returns(categoryName);
+
+            stubFactory.Setup(c => c.CreateCategory(categoryName)).Returns(stubCategory.Object);
+
+            var mockEngine = new MockedCosmeticEngine(stubFactory.Object, stubShoppingCart.Object, stubCommandParser.Object);
+
+            // Act
+            mockEngine.Start();
+
+            // Assert
+            Assert.IsTrue(mockEngine.Categories.ContainsKey(categoryName));
+            Assert.AreSame(stubCategory.Object, mockEngine.Categories[categoryName]);
+        }
+    }
+}
---
+++ C Sharp/C# Unit Testing/03. WorkShops/01. Cosmetics/Solution/Cosmetics.Tests/Products/ShampooTests.cs
@@ -0,0 +1,35 @@
+﻿using Cosmetics.Common;
+using Cosmetics.Products;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Tests.Products
+{
+    [TestFixture]
+    public class ShampooTests
+    {
+        [Test]
+        public void ShampooPrint_ShouldReturnStringInTheRerquiredFormat()
+        {
+            // Arrange
+            var shampoo = new Shampoo("Shampoo", "Head", 10M, GenderType.Men, 100, UsageType.EveryDay);
+
+            var expectedResult = new StringBuilder();
+            expectedResult.AppendLine("- Head - Shampoo:");
+            expectedResult.AppendLine("  * Price: $1000");
+            expectedResult.AppendLine("  * For gender: Men");
+            expectedResult.AppendLine("  * Quantity: 100 ml");
+            expectedResult.AppendLine("  * Usage: EveryDay");
+
+            // Act
+            var result = shampoo.Print();
+
+            // Assert
+            Assert.AreEqual(expectedResult.ToString(), result);
+        }
+    }
+}
