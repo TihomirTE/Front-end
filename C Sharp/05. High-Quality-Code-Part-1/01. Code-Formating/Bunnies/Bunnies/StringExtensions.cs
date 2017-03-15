5445f92ae3c6a793d7da79fdff4c3163625be274
TihomirTE <tihomirteofilov@gmail.com>
2017-03-15 14:41:51 +0200

    solved problems - Code-Formating

---
+++ C Sharp/05. High-Quality-Code-Part-1/01. Code-Formating/Bunnies/Bunnies/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Bunnies
+{
+    using System;
+    using System.Text;
+
+    public static class StringExtensions
+    {
+        public static string SplitToSeparateWordsByUppercaseLetter(this string sequence)
+        {
+            var probableStringMargin = 10;
+            var probableStringSize = sequence.Length + probableStringMargin;
+
+            var builder = new StringBuilder(probableStringSize);
+            var singleWhitespace = ' ';
+
+            foreach (var letter in sequence)
+            {
+                if (Char.IsUpper(letter))
+                {
+                    builder.Append(singleWhitespace);
+                }
+
+                builder.Append(letter);
+            }
+
+            return builder.ToString().Trim();
+        }
+    }
+}
