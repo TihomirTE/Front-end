3e041c121776423fcb89a9991889effb4373fe3b
TihomirTE <tihomirteofilov@gmail.com>
2017-03-23 09:19:58 +0200

    add HW High-Quality-Classes

---
+++ C Sharp/05. High-Quality-Code-Part-1/07. High-quality Classes/Inheritance-and-Polymorphism/OffsiteCourse.cs
@@ -0,0 +1,59 @@
+﻿namespace InheritanceAndPolymorphism
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class OffsiteCourse : Course
+    {
+        private string town;
+
+        public OffsiteCourse(string name)
+            : base(name)
+        {
+        }
+
+        public OffsiteCourse(string name, string teacherName)
+            : base(name, teacherName)
+        {
+        }
+
+        public OffsiteCourse(string name, string teacherName, IList<string> students)
+            : base(name, teacherName, students)
+        {
+        }
+
+        public string Town
+        {
+            get
+            {
+                return this.town;
+            }
+
+            internal set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Town can not be null or empty");
+                }
+
+                this.town = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("OffsiteCourse { Name = ");
+            result.Append(base.ToString());
+
+            if (this.Town != null)
+            {
+                result.Append("; Town = ");
+                result.Append(this.Town);
+            }
+
+            return result.ToString();
+        }
+    }
+}
