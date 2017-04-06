0090f6efc948dd55825c4948415bfc78d3f377d4
TihomirTE <tihomirteofilov@gmail.com>
2017-04-06 18:30:54 +0300

    Add Task 1. GeoLocation

---
+++ Front-end/06. JavaScript-Applications/01. Promises and asynchronous programming/Task 1. GeoLocation/location.js
@@ -0,0 +1,40 @@
+(function() {
+
+    var location = new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                resolve(position);
+            },
+            function(error) {
+                reject(error);
+            });
+    });
+
+    function coordinates(position) {
+        return {
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+        };
+    }
+
+    function createMapView(coords) {
+        let map = document.getElementById("map"),
+            src = "http://maps.googleapis.com/maps/api/staticmap?center=" +
+            coords.lat +
+            "," +
+            coords.long +
+            "&zoom=18&size=500x500&sensor=false";
+
+        map.setAttribute("src", src);
+    }
+
+    function locationNotFound(error) {
+        map.innerText = error.message;
+    }
+
+    location
+        .then(coordinates)
+        .then(createMapView)
+        .catch(locationNotFound);
+
+}());
