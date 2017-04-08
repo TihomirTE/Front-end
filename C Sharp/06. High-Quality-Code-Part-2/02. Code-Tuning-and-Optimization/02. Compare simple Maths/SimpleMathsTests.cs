c04ef316133a62083e4906c1671d3739f5c456ff
TihomirTE <tihomirteofilov@gmail.com>
2017-04-08 09:54:56 +0300

    Add explanation comments

--- C Sharp/06. High-Quality-Code-Part-2/02. Code-Tuning-and-Optimization/02. Compare simple Maths/SimpleMathsTests.cs
+++ C Sharp/06. High-Quality-Code-Part-2/02. Code-Tuning-and-Optimization/02. Compare simple Maths/SimpleMathsTests.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.Compare_simple_Maths
 {
@@ -11,8 +7,12 @@ namespace _02.Compare_simple_Maths
     {
         private const int NumberOfIterations = 1000000;
 
-        //Func<T1, T2, ..., Tn, Tr> represents a function, 
-        //that takes (T1, T2, ..., Tn) arguments and returns Tr.
+        /// <summary>
+        /// Func<T1, T2, ..., Tn, Tr> represents a function, 
+        /// that takes (T1, T2, ..., Tn) arguments and returns Tr.
+        /// </summary>
+        /// <param name="calculateFunc"></param>
+        /// <returns> Calculate time for every value</returns>
         private static TimeSpan Timer<T>(Func<T, T, T> calculateFunc)
         {
             var stopWatch = new Stopwatch();
@@ -27,9 +27,12 @@ namespace _02.Compare_simple_Maths
             return stopWatch.Elapsed;
         }
 
+        /// <summary>
+        /// Timer<T>(Func<a, b, (a (arithmetic operation) b)>)
+        /// </summary>
         public static void Main()
         {
-            Console.WriteLine("Add:");
+            Console.WriteLine("Measure performance of - Add:");
             Console.WriteLine($"Int: {Timer<int>((a, b) => (a + b))}");
             Console.WriteLine($"Long: {Timer<long>((a, b) => (a + b))}");
             Console.WriteLine($"Float: {Timer<float>((a, b) => (a + b))}");
@@ -37,7 +40,7 @@ namespace _02.Compare_simple_Maths
             Console.WriteLine($"Decimal: {Timer<decimal>((a, b) => (a + b))}");
             Console.WriteLine();
 
-            Console.WriteLine("Substract:");
+            Console.WriteLine("Measure performance of - Substract:");
             Console.WriteLine($"Int: {Timer<int>((a, b) => (a - b))}");
             Console.WriteLine($"Long: {Timer<long>((a, b) => (a - b))}");
             Console.WriteLine($"Float: {Timer<float>((a, b) => (a - b))}");
@@ -45,7 +48,7 @@ namespace _02.Compare_simple_Maths
             Console.WriteLine($"Decimal: {Timer<decimal>((a, b) => (a - b))}");
             Console.WriteLine();
 
-            Console.WriteLine("Increment:");
+            Console.WriteLine("Measure performance of - Increment:");
             Console.WriteLine($"Int: {Timer<int>((a, b) => (a++))}");
             Console.WriteLine($"Long: {Timer<long>((a, b) => (a++))}");
             Console.WriteLine($"Float: {Timer<float>((a, b) => (a++))}");
@@ -53,7 +56,7 @@ namespace _02.Compare_simple_Maths
             Console.WriteLine($"Decimal: {Timer<decimal>((a, b) => (a++))}");
             Console.WriteLine();
 
-            Console.WriteLine("multiply:");
+            Console.WriteLine("Measure performance of - Multiply:");
             Console.WriteLine($"Int: {Timer<int>((a, b) => (a * b))}");
             Console.WriteLine($"Long: {Timer<long>((a, b) => (a * b))}");
             Console.WriteLine($"Float: {Timer<float>((a, b) => (a * b))}");
@@ -61,15 +64,14 @@ namespace _02.Compare_simple_Maths
             Console.WriteLine($"Decimal: {Timer<decimal>((a, b) => (a * b))}");
             Console.WriteLine();
 
-            // increment ++b cannot divide to 0
-            Console.WriteLine("divide:");
+            // increment ++b because cannot divide to 0
+            Console.WriteLine("Measure performance of - Divide:");
             Console.WriteLine($"Int: {Timer<int>((a, b) => (a / ++b))}");
             Console.WriteLine($"Long: {Timer<long>((a, b) => (a / ++b))}");
             Console.WriteLine($"Float: {Timer<float>((a, b) => (a / ++b))}");
             Console.WriteLine($"Double: {Timer<double>((a, b) => (a / ++b))}");
             Console.WriteLine($"Decimal: {Timer<decimal>((a, b) => (a / ++b))}");
             Console.WriteLine();
-
         }
     }
 }
