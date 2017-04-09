0bbd458f64486f266903da4bf679436ef3bb61e6
TihomirTE <tihomirteofilov@gmail.com>
2017-04-09 11:19:39 +0300

    Optimize EarthRotation method

--- C Sharp/06. High-Quality-Code-Part-2/02. Code-Tuning-and-Optimization/01. Solar System/OrbitsCalculator.cs
+++ C Sharp/06. High-Quality-Code-Part-2/02. Code-Tuning-and-Optimization/01. Solar System/OrbitsCalculator.cs
@@ -117,13 +117,10 @@ namespace SolarSystem
             Update("EarthOrbitPositionY");
         }
 
+        // Optimize this method - remove the forloop
         private void EarthRotation()
         {
-            for (decimal step = 0; step <= 360; step += 0.00005m)
-            {
-                this.EarthRotationAngle = ((double)step) * Days / EarthRotationPeriod;
-            }
-
+            this.EarthRotationAngle = 360 * Days / EarthRotationPeriod;
             Update("EarthRotationAngle");
         }
 
