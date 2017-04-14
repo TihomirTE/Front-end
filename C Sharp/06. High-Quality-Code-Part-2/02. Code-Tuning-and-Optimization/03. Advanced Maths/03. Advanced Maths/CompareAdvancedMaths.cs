ad0eae95bd8961241e43c372d1475e295b704f42
TihomirTE <tihomirteofilov@gmail.com>
2017-04-14 13:38:43 +0300

    Maintain StyleCop requirements

--- C Sharp/06. High-Quality-Code-Part-2/02. Code-Tuning-and-Optimization/03. Advanced Maths/03. Advanced Maths/CompareAdvancedMaths.cs
+++ C Sharp/06. High-Quality-Code-Part-2/02. Code-Tuning-and-Optimization/03. Advanced Maths/03. Advanced Maths/CompareAdvancedMaths.cs
@@ -7,26 +7,6 @@ namespace _03.Advanced_Maths
     {
         private const int NumberOfIterations = 10000000;
 
-        /// <summary>
-        /// Func<T1, Tr> represents a function, 
-        /// that takes T1 argument and returns Tr.
-        /// </summary>
-        /// <param name="calculateFunc"></param>
-        /// <returns> Calculate time for every value</returns>
-        private static TimeSpan Timer<T>(Func<T, T> calculateFunc)
-        {
-            var stopWatch = new Stopwatch();
-            stopWatch.Start();
-
-            for (int i = 0; i < NumberOfIterations; i++)
-            {
-                calculateFunc(default(T));
-            }
-
-            stopWatch.Stop();
-            return stopWatch.Elapsed;
-        }
-
         public static void Main()
         {
             Console.WriteLine("Measure performance of - Square root");
@@ -46,6 +26,26 @@ namespace _03.Advanced_Maths
             Console.WriteLine($"Double: {Timer<double>(a => Math.Log(a))}");
             Console.WriteLine($"Decimal: {Timer<decimal>(a => (decimal)Math.Log((double)++a))}");
             Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Func<T1, Tr> represents a function, 
+        /// that takes T1 argument and returns Tr.
+        /// </summary>
+        /// <param name="calculateFunc"></param>
+        /// <returns> Calculate time for every value</returns>
+        private static TimeSpan Timer<T>(Func<T, T> calculateFunc)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for (int i = 0; i < NumberOfIterations; i++)
+            {
+                calculateFunc(default(T));
+            }
+
+            stopWatch.Stop();
+            return stopWatch.Elapsed;
         }
     }
 }
