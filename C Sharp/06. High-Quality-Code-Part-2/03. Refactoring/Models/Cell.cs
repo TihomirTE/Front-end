04c90aece4d990a17c0fde31f60cca51255fa122
TihomirTE <tihomirteofilov@gmail.com>
2017-04-21 14:02:53 +0300

    Add class Cell

---
+++ C Sharp/06. High-Quality-Code-Part-2/03. Refactoring/Models/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GameFifteen.Models
+{
+    public class Cell
+    {
+        public Cell(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public static Cell operator +(Cell currentCell, Cell nextCell)
+        {
+            if (currentCell == null || nextCell == null)
+            {
+                throw new ArgumentNullException("Cells can not be null");
+            }
+
+            return new Cell(currentCell.X + nextCell.X, currentCell.Y + nextCell.Y);
+        }
+    }
+}
