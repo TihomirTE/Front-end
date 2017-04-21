016f6b7d696c601dfbd57bbccf9f6be11087553c
TihomirTE <tihomirteofilov@gmail.com>
2017-04-21 14:34:18 +0300

    Add Enums/Direction

---
+++ C Sharp/06. High-Quality-Code-Part-2/03. Refactoring/Enums/Direction.cs
@@ -0,0 +1,14 @@
+﻿namespace GameFifteen.Enums
+{
+    public enum Direction
+    {
+        DownRight,
+        Down,
+        DownLeft,
+        Left,
+        UpLeft,
+        Up,
+        UpRight,
+        Right
+    }
+}
