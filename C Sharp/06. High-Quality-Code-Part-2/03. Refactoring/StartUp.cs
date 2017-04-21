29155b0c4e026cc4eb8d910dbd5517248f22517b
TihomirTE <tihomirteofilov@gmail.com>
2017-04-21 14:14:45 +0300

    Refactor StartUp

--- C Sharp/06. High-Quality-Code-Part-2/03. Refactoring/StartUp.cs
+++ C Sharp/06. High-Quality-Code-Part-2/03. Refactoring/StartUp.cs
@@ -1,167 +1,24 @@
 ﻿using System;
+using GameFifteen.Models;
 
 namespace GameFifteen
 {
-    public class WalkInMatrix
+    public class StartUp
     {
-        static void change(ref int dx, ref int dy)
+        static void Main()
         {
-            int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
-            int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
-
-            int cd = 0;
-            for (int count = 0; count < 8; count++)
-            {
-                if (dirX[count] == dx && dirY[count] == dy)
-                {
-                    cd = count;
-                    break;
-                }
-            }
-
-            if (cd == 7)
-            {
-                dx = dirX[0];
-                dy = dirY[0];
-                return;
-            }
-
-            dx = dirX[cd + 1];
-            dy = dirY[cd + 1];
-        }
-
-        static bool proverka(int[,] arr, int x, int y)
-        {
-            int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
-            int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
-
-            for (int i = 0; i < 8; i++)
-            {
-                if (x + dirX[i] >= arr.GetLength(0) || (x + dirX[i] < 0))
-                {
-                    dirX[i] = 0;
-                }
-
-                if (y + dirY[i] >= arr.GetLength(0) || (y + dirY[i] < 0))
-                {
-                    dirY[i] = 0;
-                }
-            }
-
-            for (int i = 0; i < 8; i++)
-            {
-                if (arr[(x + dirX[i]), (y + dirY[i])] == 0)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        static void find_cell(int[,] arr, out int x, out int y)
-        {
-            x = 0;
-            y = 0;
-
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(0); j++)
-                    if (arr[i, j] == 0)
-                    {
-                        x = i;
-                        y = j;
-
-                        return;
-                    }
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            int n;
+            int input;
 
             do
             {
                 Console.WriteLine("Enter number between 1 and 100");
-                n = int.Parse(Console.ReadLine());
-            } while (n < 1 || n > 100);
+                input = int.Parse(Console.ReadLine());
+            } while (input < 1 || input > 100);
 
-            var matrix = new int[n, n];
-            int step = n;
-            int k = 1;
-            int i = 0;
-            int j = 0;
-            int dx = 1;
-            int dy = 1;
-
-            while (true)
-            {
-                matrix[i, j] = k;
-
-                if (!proverka(matrix, i, j))
-                {
-                    break;
-                } // prekusvame ako sme se zadunili
-
-                if (i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrix[i + dx, j + dy] != 0)
-                {
-                    while (i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrix[i + dx, j + dy] != 0)
-                    {
-                        change(ref dx, ref dy);
-                    }
-                }
-                i += dx;
-                j += dy;
-                k++;
-            }
-
-            for (int p = 0; p < n; p++)
-            {
-                for (int q = 0; q < n; q++)
-                {
-                    Console.Write("{0,3}", matrix[p, q]);
-                }
-                Console.WriteLine();
-            }
-
-            find_cell(matrix, out i, out j);
-
-            if (i != 0 && j != 0)
-            { // taka go napravih, zashtoto funkciqta ne mi davashe da ne si definiram out parametrite
-                dx = 1;
-                dy = 1;
-
-                while (true)
-                { //malko e kofti tova uslovie, no break-a raboti 100% : )
-                    matrix[i, j] = k;
-                    if (!proverka(matrix, i, j))
-                    {
-                        break;
-                    }// prekusvame ako sme se zadunili
-
-                    if (i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrix[i + dx, j + dy] != 0)
-                    {
-                        while (i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrix[i + dx, j + dy] != 0)
-                        {
-                            change(ref dx, ref dy);
-                        }
-                    }
-
-                    i += dx;
-                    j += dy;
-                    k++;
-                }
-            }
-
-            for (int pp = 0; pp < n; pp++)
-            {
-                for (int qq = 0; qq < n; qq++)
-                {
-                    Console.Write("{0,3}", matrix[pp, qq]);
-                }
-                Console.WriteLine();
-            }
+            Matrix matrix = new Matrix(input);
+            Console.WriteLine(matrix);
         }
     }
 }
+
+           
