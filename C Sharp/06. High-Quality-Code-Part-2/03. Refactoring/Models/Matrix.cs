da66444fdba387d19803d091cc82e1d14c8f7ae0
TihomirTE <tihomirteofilov@gmail.com>
2017-04-21 13:56:16 +0300

    Add class Matrix

---
+++ C Sharp/06. High-Quality-Code-Part-2/03. Refactoring/Models/Matrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameFifteen.Models
+{
+    public class Matrix
+    {
+        private int rows;
+        private int cols;
+
+        public Matrix(int size)
+        {
+            this.Rows = size;
+            this.Cols = size;
+        }
+
+        public int Rows
+        {
+            get
+            {
+                return this.rows;
+            }
+
+            private set
+            {
+                this.rows = value;
+            }
+        }
+
+        public int Cols
+        {
+            get
+            {
+                return this.cols;
+            }
+
+            private set
+            {
+                this.cols = value;
+            }
+        }
+
+
+    }
+}
