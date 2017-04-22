a9164ba3123c17eb4f2c008d2d02c73cfb0acdfd
TihomirTE <tihomirteofilov@gmail.com>
2017-04-22 21:49:25 +0300

    Refactor the input logic

--- C Sharp/06. High-Quality-Code-Part-2/03. Refactoring/StartUp.cs
+++ C Sharp/06. High-Quality-Code-Part-2/03. Refactoring/StartUp.cs
@@ -7,12 +7,10 @@
         public static void Main()
         {
             int input;
-
-            do
-            {
+            
                 Console.WriteLine("Enter number between 1 and 100");
                 input = int.Parse(Console.ReadLine());
-            } while (input <= 0 || input > 100);
+           
 
             Matrix matrix = new Matrix(input);
             Console.WriteLine(matrix);
