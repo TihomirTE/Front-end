583b6ea3561efa40ec8128163d55b616b68e4836
TihomirTE <tihomirteofilov@gmail.com>
2017-04-24 15:34:02 +0300

    Add tests for Cell

---
+++ C Sharp/06. High-Quality-Code-Part-2/03. Refactoring/MatrixTests/ModelsTests/CellTests.cs
@@ -0,0 +1,41 @@
+﻿using GameFifteen.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixTests
+{
+
+    [TestFixture]
+    public class CellTests
+    {
+        [Test]
+        public void AddCorrectParameters_ConstructorShouldReturnCellReference()
+        {
+            // Arrange & Act
+            int x = 2;
+            int y = 2;
+            var cell = new Cell(x, y);
+
+            // Assert
+            Assert.IsInstanceOf<Cell>(cell);
+        }
+
+        [Test]
+        public void OperatorSum_ShouldIncreaseProperlyTheValueOfThoCells()
+        {
+            // Arrange
+            var currentCell = new Cell(2, 3);
+            var nextCell = new Cell(3, 4);
+
+            // Act & Assert
+            Assert.AreEqual(5, currentCell.X + nextCell.X);
+            Assert.AreEqual(7, currentCell.Y + nextCell.Y);
+        }
+
+
+    }
+}
