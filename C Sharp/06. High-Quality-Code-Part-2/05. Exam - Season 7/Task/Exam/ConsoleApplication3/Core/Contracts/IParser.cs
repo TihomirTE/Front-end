439326f07ffcd5a86cb61f89a67b93538636398f
TihomirTE <tihomirteofilov@gmail.com>
2017-05-03 12:04:17 +0300

    Add Providers interfaces

---
+++ C Sharp/06. High-Quality-Code-Part-2/05. Exam - Season 7/Task/Exam/ConsoleApplication3/Core/Contracts/IParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Core.Contracts
+{
+    /// <summary>
+    /// Interface for a Parser provider
+    /// </summary>
+    public interface IParser
+    {
+        //ICommand Parse
+    }
+}
---
+++ C Sharp/06. High-Quality-Code-Part-2/05. Exam - Season 7/Task/Exam/ConsoleApplication3/Core/Contracts/IWriter.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolSystem.Core.Contracts
+{
+    public interface IWriter
+    {
+        void Write(string message);
+
+        void WriteLine(string message);
+    }
+}
