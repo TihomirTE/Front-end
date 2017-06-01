fa2796ec05377447dcfc11a1e977d97b3d48ea14
TihomirTE <tihomirteofilov@gmail.com>
2017-06-01 22:08:44 +0300

    Initial EF-HW

---
+++ C Sharp/08. Databases/05. Entity Framework/Entity-Framework/Entity-Framework/CRUD-Operations.cs
@@ -0,0 +1,35 @@
+﻿using Entity_Framework.Nortwind;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework
+{
+    public class CRUD_Operations
+    {
+        public static string AddCustomer(string customerId, string companyName, string contactName, string contactTitle,
+            string address, string city, string region, string postalCode, string phone, string fax)
+        {
+            var northwind = new NorthwndEntities();
+            var customer = new Customer()
+            {
+                CustomerID = customerId,
+                CompanyName = companyName,
+                ContactName = contactName,
+                ContactTitle = contactTitle,
+                Address = address,
+                City = city,
+                Region = region,
+                PostalCode = postalCode,
+                Phone = phone,
+                Fax = fax,
+            };
+
+            northwind.Customers.Add(customer);
+            northwind.SaveChanges();
+            return customer.CustomerID;
+        }
+    }
+}
---
+++ C Sharp/08. Databases/05. Entity Framework/Entity-Framework/Entity-Framework/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using Entity_Framework.Nortwind;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            // Task 1
+            using (var dbContext = new NorthwndEntities())
+            {
+                var categories = dbContext
+                    .Categories
+                    .Where(c => c.CategoryID == 1)
+                    .Select(c => c.CategoryName)
+                    .ToList();
+
+                Console.WriteLine(categories.FirstOrDefault());
+            }
+
+            // Task 2
+            CRUD_Operations.AddCustomer("DB", "Telerik", "Gosho", "Merindjei", "Mladost",
+                                        "Sofia", "Sofia", "1715", "0888121212", "1234");
+
+        }
+    }
+}
