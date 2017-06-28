482427428641dce930b8d0d6eac421212b775fea
TihomirTE <tihomirteofilov@gmail.com>
2017-06-28 13:45:36 +0300

    Add demos Recursion

---
+++ DSA/02. Demos/02. Recursion/01. Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace _01.Factorial
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Factorial(50));
+        }
+
+        private static BigInteger Factorial(int n)
+        {
+            return Factorial(n, 1);
+        }
+
+        private static BigInteger Factorial(int n, BigInteger number)
+        {
+            if (n == 0)
+            {
+                return number;
+            }
+
+            return Factorial(n - 1, n * number);
+        }
+    }
+}
