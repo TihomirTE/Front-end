bf135655e4718255276b2c335622164c1a4dce40
TihomirTE <tihomirteofilov@gmail.com>
2017-08-07 00:51:11 +0300

    Add task explanation Messages

---
+++ DSA/04. ExamPreparation/01. Recursion/03. Messages_In_Bottle/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Messages_In_Bottle
+{
+    public class Messages
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
