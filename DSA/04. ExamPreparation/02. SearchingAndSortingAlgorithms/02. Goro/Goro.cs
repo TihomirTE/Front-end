f00e19bb9bb0aa6c830740e0ed3f816b78d22b86
TihomirTE <tihomirteofilov@gmail.com>
2017-08-08 20:08:40 +0300

    Add task Goro

---
+++ DSA/04. ExamPreparation/02. SearchingAndSortingAlgorithms/02. Goro/Goro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Goro
+{
+    public class Goro
+    {
+        public static void Main()
+        {
+            var list = new List<int>();
+            int num1 = int.Parse(Console.ReadLine());
+            list.Add(num1);
+            int num2 = int.Parse(Console.ReadLine());
+            list.Add(num2);
+            int num3 = int.Parse(Console.ReadLine());
+            list.Add(num3);
+
+            int n = int.Parse(Console.ReadLine());
+            int points = 0;
+            int maxValue = 0;
+            int index = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                maxValue = list.Max();
+                points += maxValue;
+                index = list.IndexOf(maxValue);
+                maxValue = list[index];
+                if (maxValue >= 1)
+                {
+                    maxValue--;
+                    list[index] = maxValue;
+                }
+            }
+
+            Console.WriteLine(points);
+        }
+    }
+}
