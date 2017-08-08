48faffb7d405ddf8cec755bc863c1d45ae7ac99b
TihomirTE <tihomirteofilov@gmail.com>
2017-08-08 21:45:41 +0300

    Add task Anagrami

---
+++ DSA/04. ExamPreparation/02. SearchingAndSortingAlgorithms/03. Anagrami/Anagrami.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Anagrami
+{
+    public class Anagrami
+    {
+        public static void Main()
+        {
+            string input;
+            string group = " ";
+            var list = new List<string>();
+            int numberOfGroups = 0;
+
+            do
+            {
+                input = Console.ReadLine();
+
+                // Sort Alphabetically
+                group = String.Concat(input.OrderBy(c => c));
+
+                if (group != "-1" && list.IndexOf(group) == -1)
+                {
+                    list.Add(group);
+
+                    numberOfGroups++;
+                }
+
+            } while (input != "-1");
+
+            Console.WriteLine(numberOfGroups);
+        }
+    }
+}
