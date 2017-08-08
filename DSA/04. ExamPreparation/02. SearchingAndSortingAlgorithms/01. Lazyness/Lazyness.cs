ddb7114cffb85fc110efba68bfd22a37692a49f2
TihomirTE <tihomirteofilov@gmail.com>
2017-08-08 19:00:10 +0300

    Add task Lazyness

---
+++ DSA/04. ExamPreparation/02. SearchingAndSortingAlgorithms/01. Lazyness/Lazyness.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Lazyness
+{
+    public class Lazyness
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var indexes = Console.ReadLine().Split(' ').Select(item => int.Parse(item)).ToArray();
+            var numbers = Console.ReadLine().Split(' ').Select(item => int.Parse(item)).ToList();
+            int start = indexes[0];
+            int end = indexes[1];
+            int len = end - start + 1;
+
+            var sortedArr = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i >= start && i <= end)
+                {
+                    sortedArr.Add(numbers[i]);
+                }
+            }
+
+            numbers.RemoveRange(start, len);
+
+            sortedArr.Sort();
+            numbers.InsertRange(start, sortedArr);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
