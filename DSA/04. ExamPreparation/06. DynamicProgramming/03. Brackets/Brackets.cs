a17f707b355cd50bc97ecd508c5696e75c9e00e9
TihomirTE <tihomirteofilov@gmail.com>
2017-08-16 21:44:43 +0300

    Add task Brackets

---
+++ DSA/04. ExamPreparation/06. DynamicProgramming/03. Brackets/Brackets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Brackets
+{
+    public class Brackets
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            Console.WriteLine(input.Length - 1);
+        }
+    }
+}
