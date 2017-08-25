08d690bf51a1a2063e57ca83a57e4ca4433c121c
TihomirTE <tihomirteofilov@gmail.com>
2017-08-25 20:57:47 +0300

    Add logic for Frames task

---
+++ DSA/04. ExamPreparation/07. Exam_2013/01. Frames/Frames.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Frames
+{
+    class Frames
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            var frames = new List<Frame>();
+
+            for (int i = 0; i < rows; i++)
+            {
+                var sizes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                frames.Add(new _01.Frames.Frames.Frame(sizes[0], sizes[1]));
+            }
+
+        }
+
+        struct Frame
+        {
+            public Frame(int a, int b)
+            {
+                A = a;
+                B = b;
+            }
+
+            public int A { get; set; }
+            public int B { get; set; }
+        }
+    }
+}
