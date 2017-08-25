7da964535d44c43ba96cacd737049318bf56c153
TihomirTE <tihomirteofilov@gmail.com>
2017-08-25 21:03:01 +0300

    Add Permutation and Swap

--- DSA/04. ExamPreparation/07. Exam_2013/01. Frames/Frames.cs
+++ DSA/04. ExamPreparation/07. Exam_2013/01. Frames/Frames.cs
@@ -16,11 +16,36 @@ namespace _01.Frames
             for (int i = 0; i < rows; i++)
             {
                 var sizes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-                frames.Add(new _01.Frames.Frames.Frame(sizes[0], sizes[1]));
+                frames.Add(new Frame(sizes[0], sizes[1]));
             }
 
         }
 
+        static void Perm<T>(T[] arr, int k)
+        {
+            if (k >= arr.Length)
+            {
+
+            }
+            else
+            {
+                Perm(arr, k + 1);
+                for (int i = k + 1; i < arr.Length; i++)
+                {
+                    Swap(ref arr[k], ref arr[i]);
+                    Perm(arr, k + 1);
+                    Swap(ref arr[k], ref arr[i]);
+                }
+            }
+        }
+
+        static void Swap<T>(ref T first, ref T second)
+        {
+            T oldFirst = first;
+            first = second;
+            second = oldFirst;
+        }
+        
         struct Frame
         {
             public Frame(int a, int b)
