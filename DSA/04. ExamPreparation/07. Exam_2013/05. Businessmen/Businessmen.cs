81192cbf458e94a500dfad791246d04bbec2ab22
TihomirTE <tihomirteofilov@gmail.com>
2017-08-27 17:48:06 +0300

    Add task Businessmen

---
+++ DSA/04. ExamPreparation/07. Exam_2013/05. Businessmen/Businessmen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05.Businessmen
+{
+    public class Businessmen
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var dp = new long[71];
+            dp[0] = 1;
+
+            // f(n) += f(oneSidePeople) * f(numberOfBusinessmen - oneSidePeople - 2)
+            for (int allPeople = 2; allPeople <= n; allPeople += 2)
+            {
+                for (int oneSide = allPeople - 2; oneSide >= 0; oneSide -= 2)
+                {
+                    dp[allPeople] += dp[oneSide] * dp[allPeople - oneSide - 2];
+                }
+            }
+
+            for (int i = 0; i < n; i+=2)
+            {
+                Console.WriteLine(dp[i]);
+            }
+
+            //Console.WriteLine(dp[n]);
+        }
+    }
+}
