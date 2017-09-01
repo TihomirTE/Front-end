b2cefcc89a7fde95996ddd9b32a2a538877edc6f
TihomirTE <tihomirteofilov@gmail.com>
2017-09-01 12:20:51 +0300

    Add task Rings(solved with Variation)

---
+++ DSA/04. ExamPreparation/09. Exam_2016/03. TheRings/Rings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.TheRings
+{
+    public class Rings
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var input = new List<int>();
+            var listResult = new List<int>();
+            int counter = 1;
+            int result = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                input.Add(int.Parse(Console.ReadLine()));
+            }
+
+            input.Sort();
+
+            for (int i = 1; i < n; i++)
+            {
+                if (input[i - 1] == input[i])
+                {
+                    counter += 1;
+                    listResult.Add(counter);
+                }
+                else
+                {
+                    counter = 1;
+                }
+            }
+
+            for (int i = 0; i < listResult.Count; i++)
+            {
+                result *= listResult[i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
