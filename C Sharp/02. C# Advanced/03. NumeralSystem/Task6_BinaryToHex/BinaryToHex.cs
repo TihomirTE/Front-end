15f680aaeb540fff65988e1c354601e0c7062eae
Tihomir Teofilov <tihomirteofilov@gmail.com>
2019-05-30 17:06:10 +0300

    Implement Binary to Hexadecimal conversion

--- C Sharp/02. C# Advanced/03. NumeralSystem/Task6_BinaryToHex/BinaryToHex.cs
+++ C Sharp/02. C# Advanced/03. NumeralSystem/Task6_BinaryToHex/BinaryToHex.cs
@@ -1,48 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Task6_BinaryToHex
+namespace vsTests
 {
-    class BinaryToHex
+    class Program
     {
-        static void Main()
+        public static void Main()
         {
+            char[] binary = Console.ReadLine().ToCharArray();
+            // variant 1
+            //string hex = Convert.ToInt64(binary, 2).ToString("X"); // -> it can be solved only with this code :)
 
-            string binary = Console.ReadLine();
-            //string hex = Convert.ToInt64(binary, 2).ToString("X"); -> it can be solved only with this code :)
-
-            // 1.2v 
-            string hex = ConvertBinaryToHex(binary);
-
+            // Variant 2
+            //1) convert Binary to Decimal
+            long decimalNumber = ConvertBinaryToDecimal(binary);
+            //2) convert Decimal to Hexadecimal
+            string hex = ConvertDecimalToHex(decimalNumber);
+            Console.WriteLine(hex);
         }
 
-        private static string ConvertBinaryToHex(string binary)
+        private static long ConvertBinaryToDecimal(char[] arr)
         {
-            int num = 0;
-            string hex = string.Empty;
-            int counter = 0;
-            for (int i = binary.Length - 1; i >= 0; i --)
+            long number = 0;
+            for (long i = arr.Length - 1; i >= 0; i--)
             {
-                if ((i + 1) % 4 == 0 && i != binary.Length - 1)
+                number += (arr[i] - '0') * (long)Math.Pow(2, (arr.Length - i - 1));
+            }
+            
+            return number;
+        }
+
+        static string ConvertDecimalToHex(long number)
+        {
+            StringBuilder hexNumber = new StringBuilder();
+            
+            while (number > 0)
+            {
+                int remainder = (int)(number % 16);
+                if (remainder < 10)
                 {
-                    hex += Convert.ToString(num, 16);
-                }
-                if ((i + 1) % 4 == 0)
-                {
-                    num = 0;
-                    
-                    num = (binary[i] & 1) * 8;
+                    hexNumber.Insert(0, remainder);
                 }
                 else
                 {
-                    num += (binary[i] & 1) * (int)Math.Pow(2, (i % ));
+                    switch (remainder)
+                    {
+                        case 10:
+                            hexNumber.Insert(0, "A");
+                        break;
+                        case 11:
+                            hexNumber.Insert(0, "B");
+                         break;
+                        case 12:
+                            hexNumber.Insert(0, "C");
+                        break;
+                        case 13:
+                            hexNumber.Insert(0, "D");
+                        break;
+                        case 14: 
+                            hexNumber.Insert(0, "E");
+                        break;
+                        case 15: 
+                            hexNumber.Insert(0, "F");
+                        break;
+                        
+                        default:
+                         break;
+                    }
                 }
-                
+                number /= 16;
             }
-            return hex;
+
+            return hexNumber.ToString();
         }
     }
 }
